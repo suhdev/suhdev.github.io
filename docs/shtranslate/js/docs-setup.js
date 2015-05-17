5d4c3716d664cf7ef1c49b6f4ca08fa3e8c7e7fa
Suhail Abood <suhail.abood@gmail.com>
2015-05-17 05:55:55 +0100

    Added shTranslate documentation

---
+++ docs/shtranslate/js/docs-setup.js
@@ -0,0 +1,81 @@
+NG_DOCS={
+  "sections": {
+    "api": "ngSuh"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "module:shTranslate",
+      "shortName": "shTranslate",
+      "type": "overview",
+      "moduleName": "shTranslate",
+      "shortDescription": "shTranslate",
+      "keywords": "api general module overview set shtranslate suhailabood tools"
+    },
+    {
+      "section": "api",
+      "id": "shTranslate.directive:shTranslation",
+      "shortName": "shTranslation",
+      "type": "directive",
+      "moduleName": "shTranslate",
+      "shortDescription": "Imports the required translations into the i18n object of the scope. See the example below",
+      "keywords": "$scope addrepository angular api ar-sa boxing class config container controller ctrl directive en-gb example function html i18n i18nlang i18nlangprovider imports ip js json kick kick_boxing kick_throw lang langs module ng-attr-cx ng-attr-title ng-bind ng-click ng-controller object push required scope script setfallbacklocale setlocale sh-translation shift shtranslate suh-lang-group suhail switchlanguage testctrl throw translations var"
+    },
+    {
+      "section": "api",
+      "id": "shTranslate.I18NLang",
+      "shortName": "shTranslate.I18NLang",
+      "type": "service",
+      "moduleName": "shTranslate",
+      "shortDescription": "Provides an object to create network resources",
+      "keywords": "$scope actively addrepository angular api array assuming case config console contactus controller create default desc dot en-us example fallback false format function i18langprovider i18nlang indicate ip isfallbacklocaleloaded islocaleloaded item items javascript json key label lang language lazily literal literals ll loaded loc locale log method module multpile nav navigation network object otherwsie path placeholders promise provided ready replacable replacements repositories repository resolved resources retrieves return returns separated service set setfallbacklocale setlocale sets shtranslate silly switch test testapp testctrl text translation true type values var"
+    },
+    {
+      "section": "api",
+      "id": "shTranslate.provider:I18NLangProvider",
+      "shortName": "I18NLangProvider",
+      "type": "object",
+      "moduleName": "shTranslate",
+      "shortDescription": "A translation provider which makes it easier to bootstrap the locales repositories and ",
+      "keywords": "about-us active actual addrepository adds angular api application ar-sa bootstrap changed config couldn current database databases default easier en-us fallback false files function i18langprovider i18nlangprovider initialization initialized ip it-it javascript json key lazily library literal literals load loaded loc locale locales location manually method module multpile navigation needed note object provided provider repositories repository request service set setfallbacklocale setloadlazily setlocale sets setting shtranslate single specific stage testmod translation true url urls values"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/prettify.js",
+      "js/app.min.js"
+    ],
+    "styles": [],
+    "title": "ngSuh",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "titleLink": "/api",
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true,
+      "prettify": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "angular-animate.min.js",
+    "marked.js",
+    "prettify.js",
+    "app.min.js"
+  ]
+};
